b3d5383fac77abef975cfdee1314d23fe6a260c2
Lucas Pauker <lucas.pauker@gmail.com>
2020-02-14 11:53:18 -0800

    Make timer work, add sorting

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -10,14 +10,26 @@ class Leaderboard extends React.Component {
     super(props);
     this.state = {
       user_json: [],
-      display: "individualNames"
+      display_number: 0,
+      display_options: [],
+      display_data: []
     }
+    window.setInterval(this.iterateDisplayNumber.bind(this), 8000);
+  }
+
+  iterateDisplayNumber() {
+    this.setState(state => {
+      const display_number = state.display_number + 1;
+      return {
+        display_number
+      }
+    });
   }
 
   async componentDidMount() {
-    const body = await API.get("treehacks", "/users", {});
+    const users_body = await API.get("treehacks", "/users", {});
     let user_list = [];
-    body["results"].map(
+    users_body["results"].map(
       one_user_json =>
         one_user_json.year == 2020 &&
         one_user_json.status == "admitted" &&
@@ -25,13 +37,72 @@ class Leaderboard extends React.Component {
         user_list.push(one_user_json)
     );
     console.log(user_list);
-    this.setState({user_json: user_list});
+
+    const leaderboard_body = await API.get("treehacks", "/leaderboard", {});
+    let display_options = [];
+    for (var key in leaderboard_body["data"]) {
+      var user_id = leaderboard_body["data"][key]["data"]["data_1580776107879"]
+      var data = leaderboard_body["data"][key]["attendance"];
+      Object.keys(data).map(key =>
+        !display_options.includes(key) && display_options.push(key)
+      );
+      user_list.map(user =>
+        user_id == user["user"]["id"] && (user["attendance"] = data)
+      );
+    }
+
+    let display_data = [];
+    display_options.forEach(category => {
+      let ordered_data = [];
+      user_list.map(user => {
+        let user_picture = null;
+        if (user["forms"]["meet_info"]) {
+          user_picture = user["forms"]["meet_info"]["profilePicture"];
+        }
+        let user_name = user["forms"]["application_info"]["first_name"];
+        if (user["forms"]["application_info"]["last_name"]) {
+          user_name += " " + user["forms"]["application_info"]["last_name"].charAt(0);
+        }
+        if (user["attendance"]) {
+          ordered_data.push({
+            id: user["user"]["id"],
+            picture: user_picture,
+            name: user_name,
+            attendance: user["attendance"][category]
+          })
+        } else {
+          ordered_data.push({
+            id: user["user"]["id"],
+            picture: user_picture,
+            name: user_name,
+            attendance: 0
+          })
+        }
+      });
+      ordered_data.sort(
+        function (a,b) {
+          return b["attendance"] - a["attendance"];
+        }
+      );
+      display_data.push({
+        category: category,
+        data: ordered_data
+      });
+    });
+
+    console.log(user_list)
+    console.log(display_options)
+    console.log(display_data)
+    this.setState({user_json: user_list, display_options: display_options, display_data: display_data});
   }
 
 
   render () {
-    let columns = ["Name", "# Attended"]
     let tableClasses = ["dark", "light"];
+    let categoryIndex = this.state.display_number % this.state.display_options.length;
+    let category = this.state.display_options[categoryIndex];
+    let displayData = this.state.display_data[categoryIndex];
+    console.log(displayData);
     if (this.state.user_json.length == 0) {
       return <Loading />;
     }
@@ -44,36 +115,31 @@ class Leaderboard extends React.Component {
               <span className="logo-text-hacks">hacks</span>
               <span className="logo-text-leaderboard">leaderboard</span>
           </div>
-          <table cellspacing="0">
+          <table cellSpacing="0">
             <thead>
               <tr>
-                {columns.map(column => (
-                  <th>{column}</th>
-                ))}
+                <th>Name</th>
+                <th># <span>{category}</span> Attended</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.user_json.map((datapoint, index) => (
+              {displayData["data"].map((datapoint, index) =>
                 <tr className={tableClasses[index % 2]}>
                   <td>
-                    {datapoint["forms"]["meet_info"]["profilePicture"] && <img src={datapoint["forms"]["meet_info"]["profilePicture"]} />}
-                    {datapoint["forms"]["application_info"]["first_name"] && datapoint["forms"]["application_info"]["first_name"]} {datapoint["forms"]["application_info"]["last_name"] && datapoint["forms"]["application_info"]["last_name"].charAt(0)}
+                    <img src={datapoint["picture"]} />
+                    {datapoint["name"]}
                   </td>
                   <td>
-                    5
+                    {datapoint["attendance"]}
                   </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
       );
     }
   }
-}
-
-
-class Table extends React.Component {
 }
 
 export default Leaderboard;
