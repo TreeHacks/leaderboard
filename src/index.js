cf4ccd24bf8d817ba8312178e09ad887018d3ba4
Lucas Pauker <lucas.pauker@gmail.com>
2020-02-15 09:46:33 -0800

    remove auth

--- src/index.js
+++ src/index.js
@@ -9,85 +9,16 @@ import queryString from "query-string";
 const LOGIN_URL = process.env.REACT_APP_LOGIN_URL;
 const ENDPOINT_URL = process.env.REACT_APP_ENDPOINT_URL;
 
-export const custom_header = async () => {
-  return { Authorization: await localStorage.getItem("jwt") };
-};
-
 API.configure({
   endpoints: [
     {
       name: "treehacks",
-      endpoint: ENDPOINT_URL,
-      custom_header: custom_header
+      endpoint: ENDPOINT_URL
     }
   ]
 });
 
-export function parseJwt(token) {
-  var base64UrlSplit = token.split(".");
-  if (!base64UrlSplit) return null;
-  const base64Url = base64UrlSplit[1];
-  if (!base64Url) return null;
-  const base64 = base64Url.replace("-", "+").replace("_", "/");
-  return JSON.parse(window.atob(base64));
-}
-
-function getCurrentUser() {
-  const jwt = getJwt();
-  if (jwt) {
-    // Verify JWT here.
-    const parsed = parseJwt(jwt);
-    if (!parsed) {
-      console.log("JWT invalid");
-    } else if (new Date().getTime() / 1000 >= parseInt(parsed.exp)) {
-      console.log("JWT expired");
-      // TODO: add refresh token logic if we want here.
-    } else {
-      let attributes = {
-        name: parsed["name"],
-        email: parsed["email"],
-        email_verified: parsed["email_verified"],
-        "cognito:groups": parsed["cognito:groups"]
-      };
-      return {
-        username: parsed["sub"],
-        attributes
-      };
-    }
-  }
-  // If JWT from SAML has expired, or if there is no JWT in the first place, run this code.
-  throw "No current user";
-}
-
-function getJwt() {
-  return localStorage.getItem("jwt");
-}
-
-function logout() {
-  localStorage.removeItem("jwt");
-  window.location.href = `${LOGIN_URL}/logout?redirect=${window.location.href}`;
-}
-
-function login() {
-  window.location.href = `${LOGIN_URL}?redirect=${window.location.href}`;
-}
-
-const hash = queryString.parse(window.location.hash);
-if (hash && hash.jwt) {
-  localStorage.setItem("jwt", hash.jwt);
-  window.location.hash = "";
-}
-
 function Main() {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    try {
-      setUser(getCurrentUser());
-    } catch (e) {
-      login();
-    }
-  }, []);
-
   return (
     <Leaderboard />
   )
